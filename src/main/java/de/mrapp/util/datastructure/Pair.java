983eb89d9e69fc90c10df728a46be1ad327e5aad
Michael Rapp <michael.rapp90@googlemail.com>
2017-07-27 00:34:23 +0200

    Added comments and copyright information.

--- src/main/java/de/mrapp/util/datastructure/Pair.java
+++ src/main/java/de/mrapp/util/datastructure/Pair.java
@@ -1,21 +1,64 @@
+/*
+ * Copyright 2017 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package de.mrapp.util.datastructure;
 
-// TODO: Comments & copyright
+/**
+ * A data structure, which eases to pass around a pair of two objects. This object provides a
+ * sensible implementation of equals(), returning true if equals() is true on each of the contained
+ * objects.
+ *
+ * @param <F> The type of the first object
+ * @param <S> The type of the second object
+ * @author Michael Rapp
+ * @since 1.0.0
+ */
 public class Pair<F, S> {
 
+    /**
+     * The first object.
+     */
     public final F first;
 
+    /**
+     * The second object.
+     */
     public final S second;
 
+    /**
+     * Creates a new pair.
+     *
+     * @param first  The first object as an instance of the generic type F
+     * @param second The second object as an instance of the generic type S
+     */
     public Pair(final F first, final S second) {
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * Creates a new pair.
+     *
+     * @param <F>    The type of the first object
+     * @param <S>    The type of the second object
+     * @param first  The first object as an instance of the generic type F
+     * @param second The second object as an instance of the generic type S
+     * @return The pair, which has been created, as an instance of the class {@link Pair}. The pair
+     * may not be null
+     */
     public static <F, S> Pair<F, S> create(final F first, final S second) {
         return new Pair<>(first, second);
     }
-
 
     @Override
     public final int hashCode() {
